2bdb192e489e40340774e179345ce86a7f2601b5
Anwaroxxx <anwar.azarzou.6969@gmail.com>
2026-09-04 15:47:54 +0100

    restructure: collapse back/ and front/ into single Inertia app at root
    - Move back/* to repo root (Laravel + Inertia standard layout)
    - Move front/src/* to resources/js/* (Inertia pages/components)
    - Drop redundant standalone SPA shell (front/package.json, vite,
      public/, index.html); backend configs (package.json, .gitignore)
      win as they already contained the Inertia setup
    - No behavior changes; web, api, refactor and docs follow separately

--- front/src/actions/App/Http/Controllers/Qr/index.ts
+++
@@ -1,7 +0,0 @@
-import TokenController from './TokenController'
-
-const Qr = {
-    TokenController: Object.assign(TokenController, TokenController),
-}
-
-export default Qr
--- front/src/actions/App/index.ts
+++
@@ -1,9 +0,0 @@
-//still none since its just a combo
-import Http from './Http'
-
-const App = {
-    Http: Object.assign(Http, Http),
-    http: Object.assign(Https, Https)
-}
-
-export default App
